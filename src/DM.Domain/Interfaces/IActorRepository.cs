eded5e1edbd519630a5af366582dfc4ee07597d5
Shelgas <akozlov1997@gmail.com>
2022-07-26 12:46:34 +0300

    Added repostory_interfaces for Actror and Movie

---
+++ src/DM.Domain/Interfaces/IActorRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DM.Domain.Interfaces
+{
+    public interface IActorRepository : IRepositoryBase<Actor>
+    {
+        Task<IEnumerable<Actor>> GetAllOwnersAsync();
+        Task<Actor> GetOwnerByIdAsync(Guid actorId);
+        Task<Actor> GetOwnerWithDetailsAsync(Guid actorId);
+        void CreateOwner(Actor actor);
+        void UpdateOwner(Actor actor);
+        void DeleteOwner(Actor actor);
+    }
+}
---
+++ src/DM.Domain/Interfaces/IMovieRepository.cs
@@ -0,0 +1,19 @@
+﻿global using DM.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DM.Domain.Interfaces
+{
+    public interface IMovieRepository : IRepositoryBase<Movie>
+    {
+        Task<IEnumerable<Movie>> GetAllOwnersAsync();
+        Task<Movie> GetOwnerByIdAsync(Guid movieId);
+        Task<Movie> GetOwnerWithDetailsAsync(Guid movieId);
+        void CreateOwner(Movie movie);
+        void UpdateOwner(Movie movie);
+        void DeleteOwner(Movie movie);
+    }
+}
